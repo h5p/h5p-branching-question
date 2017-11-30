ef49bf4c49649eb1ef8c49ff0e06fadf7d1afc10
timothyylim <timothylim23@gmail.com>
2017-11-30 10:45:32 +0100

    HFP-1748 Add feedback screen

--- branchingQuestion.js
+++ branchingQuestion.js
@@ -4,24 +4,66 @@ H5P.BranchingQuestion = (function ($) {
     var self = this;
     H5P.EventDispatcher.call(self);
 
-    var wrapper = document.createElement('div');
+    var createWrapper = function(parameters) {
+      var wrapper = document.createElement('div');
 
-    var title = document.createElement('p');
-    title.innerHTML = parameters.question;
-    wrapper.append(title);
+      var title = document.createElement('p');
+      title.innerHTML = parameters.question;
+      wrapper.append(title);
 
-    for (var i = 0; i < parameters.answers.length; i++) {
-      var answer = document.createElement('ul');
-      answer.innerHTML = parameters.answers[i].text;
-      answer.nextContentId = parameters.answers[i].nextContentId;
+      return wrapper;
+    }
 
-      answer.onclick = function() {
-        self.trigger('navigated', this.nextContentId);
+    var appendMultiChoiceSection = function(parameters, wrapper) {
+      for (var i = 0; i < parameters.answers.length; i++) {
+
+        var answer = document.createElement('ul');
+        answer.innerHTML = parameters.answers[i].text;
+        answer.nextContentId = parameters.answers[i].nextContentId;
+
+        // Create feedback screens if they exist // TODO: check for undefined
+        if (parameters.answers[i].addFeedback) {
+          answer.feedbackScreen = createFeedbackScreen(parameters.answers[i].feedback, answer.nextContentId);
+        }
+
+        answer.onclick = function() {
+          if (this.feedbackScreen !== undefined) {
+            wrapper.append(this.feedbackScreen);
+          }
+          else {
+            self.trigger('navigated', this.nextContentId);
+          }
+        };
+
+        wrapper.append(answer);
+      }
+
+      return wrapper;
+    }
+
+    var createFeedbackScreen = function(feedback, nextContentId) {
+      var wrapper = document.createElement('div');
+
+      var title = document.createElement('p');
+      title.innerHTML = feedback;
+      wrapper.append(title);
+
+      var navButton = document.createElement('button');
+      navButton.onclick = function() {
+        self.trigger('navigated', nextContentId);
       };
-      wrapper.append(answer);
+
+      var text = document.createTextNode('Proceed'); // TODO: use translatable
+      navButton.append(text);
+
+      wrapper.append(navButton);
+
+      return wrapper;
     }
 
     self.attach = function ($container) {
+      var wrapper = createWrapper(parameters);
+      wrapper = appendMultiChoiceSection(parameters, wrapper);
       $container.addClass('h5p-advanced-text').html(wrapper);
     };
   }
