8c266631899334e76fff2e7eeeca9c95c1ef1042
Ravi Majithia <ravikiran.majithia@joubel.com>
2022-07-15 15:20:24 +0200

    HFP-3539 Fix support for spacebar

--- branchingQuestion.js
+++ branchingQuestion.js
@@ -66,7 +66,7 @@ H5P.BranchingQuestion = (function () {
 
       close.addEventListener('keyup', function (event) {
         // Add support for space and enter
-        if (event.code === 'Enter' || event.code === ' ') {
+        if (event.code === 'Enter' || event.code === 'Space') {
           closeDialog();
         }
       });
