0bf9da971724be74b55f899c64f3e8cb0539217a
Frode Petterson <frode.andre.petterson@gmail.com>
2022-10-20 15:44:16 +0200

    HFP-3609 Add resize when displaying feedback dialog
    Should fix issues with hidden content on mobile devices

--- branchingQuestion.js
+++ branchingQuestion.js
@@ -103,6 +103,7 @@ H5P.BranchingQuestion = (function () {
             }
             wrapper.innerHTML = '';
             wrapper.appendChild(this.feedbackScreen);
+            self.parent.trigger('resize');
             answered = index;
             timestamp = new Date().toISOString();
             this.proceedButton.focus();
